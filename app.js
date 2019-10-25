b1a9ed1734caca7d00ca1f674634f28310a487dc
Su-Yeon Bak <bsuyeon16@gmail.com>
2019-10-25 21:14:11 +0900

    using canvas context 2d api

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,22 +13,25 @@ const stopPainting = () => {
   painting = false;
 };
 
-const onMouseMove = event => {
-  const x = event.offsetX;
-  const y = event.offsetY;
-};
-
-const onMouseDown = event => {
+const startPainting = () => {
   painting = true;
 };
 
-const onMouseUp = event => {
-  stopPainting();
+const onMouseMove = event => {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 };
 
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
-  canvas.addEventListener('mousedown', onMouseDown);
-  canvas.addEventListener('mouseup', onMouseUp);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
